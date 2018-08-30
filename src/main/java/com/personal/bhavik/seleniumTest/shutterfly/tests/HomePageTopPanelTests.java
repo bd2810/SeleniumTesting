2ae684cdf392556b7e2ea3234103b8e44d9be7b0
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-08-30 11:02:37 -0400

    more changes

--- src/main/java/com/personal/bhavik/seleniumTest/shutterfly/tests/HomePageTopPanelTests.java
+++ src/main/java/com/personal/bhavik/seleniumTest/shutterfly/tests/HomePageTopPanelTests.java
@@ -2,48 +2,89 @@ package com.personal.bhavik.seleniumTest.shutterfly.tests;
 
 import java.lang.reflect.Method;
 
-import org.openqa.selenium.WebElement;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.personal.bhavik.seleniumTest.TestBaseClass;
 import com.personal.bhavik.seleniumTest.utilities.SeleniumUtilities;
+import com.personal.bhavik.seleniumTest.utilities.ShutterflyUtilities;
+import com.personal.bhavik.seleniumTest.shutterfly.pages.HomePage;
 
 public class HomePageTopPanelTests extends TestBaseClass {
 
 	private static String URL = "https://www.shutterfly.com";
 	private String testName;
 	SeleniumUtilities mySeleniumUtilities;
+	ShutterflyUtilities myShutterflyUtilities;
+	private final static Logger logger = LoggerFactory.getLogger(HomePageTopPanelTests.class);
 
-	@BeforeMethod
-	public void getTestName(Method method) {
-
-		testName = method.getName();
+	
+	@BeforeClass
+	public void beforeClass () {
+		
+		BasicConfigurator.configure();
 	}
+	
+	
+	@BeforeMethod
+	public void beforeMethod(Method method) {
 
+		testName = method.getName();		
+		logger.info("*** Beginning test: " + testName + " ***");
+	}
+	
 	@Test
 	public void testCloseModalPopUP() throws Exception {
 		
-		WebElement elem = null;
+		boolean testResult = false;
+
+		try {
+			
+			initializeWebUI(testName, URL);
+
+			if (mySeleniumUtilities.doesElementExist(HomePage.modalPopUp)) {
+
+				logger.info("Sign up popup detected");
+				mySeleniumUtilities.click(HomePage.modalPopUpClose);
+				Thread.sleep(1000);
+
+			}
+			
+			testResult = !mySeleniumUtilities.doesElementExist(HomePage.modalPopUp);
+			
+		} finally {
+			
+			assert(testResult);
+			mySeleniumUtilities.finishTest(testName, testResult);
+		}
+
+	}
+	
+	@Test
+	public void testShutterflyIcon() throws Exception {
+		
 		boolean testResult = false;
 
 		try {
 
 			initializeWebUI(testName, URL);
-			Thread.sleep(5000);
 
-			elem = mySeleniumUtilities.getElementLoose(".legacy.modal.fade.in");
-
-			if (elem.isDisplayed()) {
-				mySeleniumUtilities.getElementLoose("modal-close").click();
-				Thread.sleep(3000);
-			}
+			ShutterflyUtilities.closeModalPopUp();		
 			
-			testResult = !elem.isDisplayed();
-			assert(testResult);
+			testResult = mySeleniumUtilities.isElementDisplayed(HomePage.shutterflyIcon);
+			
 			
 		} finally {
-
+			
+			assert(testResult);
 			mySeleniumUtilities.finishTest(testName, testResult);
 		}
 
@@ -54,5 +95,14 @@ public class HomePageTopPanelTests extends TestBaseClass {
 		createWebDriver(testName, URL);
 		mySeleniumUtilities = new SeleniumUtilities(testName, getDriver());
 	}
+	
+	
+	@AfterMethod
+	public void afterMethod(Method method) {
+
+		testName = method.getName();
+		logger.info("*** Ending test: " + testName + " ***");
+	}
+
 
 }
